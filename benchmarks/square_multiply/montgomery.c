da58c51bdce085d5673b9c05223b5a096f5c6938
Jason Cai <jcai@ubuntu.(none)>
2017-11-19 16:29:11 -0600

    Added Montgomery Square and Multiply

---
+++ benchmarks/square_multiply/montgomery.c
@@ -0,0 +1,28 @@
+#include<stdint.h>
+
+int main(void){
+  volatile uint32_t x1 = 3;
+  volatile uint32_t x2 = 9;
+  volatile uint8_t n0 = 0;
+  volatile uint8_t n1 = 1;
+
+  volatile uint32_t base1 = 3;
+  volatile uint32_t base2 = 9;
+  int i;
+
+  for(i = 0; i < 5000; i ++){
+    if(n0 == 0){
+      x2 = base1 * base2;
+      x1 = base1 * base1;
+    }
+  }
+
+  for(i = 0; i < 5000; i ++){
+    if(n0 == ){
+      x2 = base1 * base2;
+      x1 = base1 * base1;
+    } else {
+      x1 = base1 * base2;
+      x2 = base2 * base2;
+    }
+}
