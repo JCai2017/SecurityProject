be31553b8e1094187165b2405c9b02c38cd30932
Jason Cai <jcai@ubuntu.(none)>
2017-11-19 15:38:37 -0600

    Added Basic and Yaoś Method for Square and Multiply

--- benchmarks/square_multiply/basic.c
+++ benchmarks/square_multiply/basic.c
@@ -1,14 +1,27 @@
 #include<stdint.h>
 
 int main(void){
-  uint32_t x = 3;
-  uint32_t n = 5;
-  uint32_t y = 1;
+  volatile uint32_t x;
+  volatile uint32_t n;
+  volatile uint32_t y;
   int i;
+  volatile uint32_t base = 3;
+  volatile uint32_t expo0 = 4;
+  volatile uint32_t expo1 = 5;
+  volatile uint32_t result = 1;
 
   for(i = 0; i < 5000; i ++){
-    
+    if(expo0 % 2 == 0){
+      x = base * base;
+      n = expo0 >> 1;
+    }
   }
 
-
+  for(i = 0; i < 5000; i ++){
+    if(expo1 % 2 == 1){
+      y = base * result;
+      x = base * base;
+      n = expo1 > 1;
+    }
+  }
 }
