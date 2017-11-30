635466b8b1e60c3bc42a34012791a7b102d60704
sbhadsavle <sarang.bhadsavle@gmail.com>
2017-11-29 21:40:50 -0600

    Generated resulting data from datapoints

--- powerTraces/datapoints/collect_power_timing.py
+++ powerTraces/datapoints/collect_power_timing.py
@@ -1,12 +1,21 @@
 
 import sys
 import csv
+import os
 
-if (len(sys.argv) < 2):
-	print("Error: supply path to input csv")
-	sys.exit(0)
+# if (len(sys.argv) < 2):
+# 	print("Error: supply path to input csv")
+# 	sys.exit(0)
 
-with open(sys.argv[1], 'r') as powercsv:
-	csvreader = csv.reader(powercsv, delimiter=',')
-	datarows = list(csvreader)
-	print(datarows[93 + 1][0]) # +1 for the header row 
+# print(os.listdir('.'))
+for datapointdir in os.listdir('.'):
+	if "." not in datapointdir:
+		with open(datapointdir + "/power_trace.csv", 'r') as powercsv:
+			csvreader = csv.reader(powercsv, delimiter=',')
+			datarows = list(csvreader)
+			# print(datarows[93 + 1][-1]) # +1 for the header row, -1 gets last element
+			with open("results.txt", 'a+') as outfile:
+				time = 1
+				while (abs(float(datarows[93+1+time][-1]) - float(datarows[93+1][-1])) < 1):
+					time += 1
+				outfile.write(datapointdir + "," + datarows[93+1][-1] + "," + str(time) + "," + str(float(datarows[93+1][-1])/float(time)) + "\n") 
