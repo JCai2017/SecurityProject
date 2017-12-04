c49d07ef7d866e5ee127c043b49eb95809adf58c
Alvin Tung <aytung94@gmail.com>
2017-12-04 15:24:23 -0600

    added LSTM classifier

---
+++ classifier/power_parser.py
@@ -0,0 +1,32 @@
+import sys
+#import matplotlib.pyplot as plt
+
+#def getValue(string)
+
+# print values out in a csv file
+with open(sys.argv[1], 'w') as f:
+	f.write("key")
+with open(sys.argv[2], 'w') as f:
+	for i in range(100):
+		if(i is 99)
+			f.write(i)
+		else 
+			f.write(i + ",")
+		
+#for i in range(16):
+#	with open(sys.argv[1], 'a') as f:
+#		f.write(
+#	with open(sys.argv[2], 'a') as f:
+#	for e in event:
+#		f.write(e + ",")
+#	f.write("\n")
+#	for i in xrange(0, len(event_values[0])):
+#		for v in event_values:
+#			f.write(v[i] + ",")
+#			summation += float(v[i])
+#		summation -= i
+#		sum_list.append(summation)
+#		f.write(str(summation))
+#		summation = 0
+#		f.write("\n")
+#
--- classifier/test.py
+++ classifier/test.py
@@ -1,5 +1,56 @@
+import numpy as np
 import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import LSTM
+from keras.preprocessing import sequence
+from keras.utils.np_utils import to_categorical
 
-df = pd.read_csv('boston.csv')
-print df['Month']
-print df.size
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+
+from matplotlib import pyplot
+
+# fix random seed for reproducibility
+np.random.seed(7)
+
+# load the dataset 
+df = pd.read_csv('test_power.csv')
+
+# Rescale it - should help network performance
+scaler = MinMaxScaler(feature_range=(0,1))
+X = scaler.fit_transform(df)
+
+y = pd.read_csv('test_key.csv')
+y = to_categorical(y, 1 << 4)
+
+# Just so we can have a validation set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=7)
+
+# This next part is because keras is picky with how they want data
+X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
+X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
+
+# truncate and pad input sequences
+# create the model
+model = Sequential()
+model.add(LSTM(25, input_shape=(X_train.shape[1], X_train.shape[2])))
+# Since it seems to be a categorical problem, use softmax activation instead of linear
+model.add(Dense(1 << 4, activation='softmax')) # 16 possible key values, activation, 16keys*4training-traces, 197us sigmoid
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy']) #rmsprop
+print(model.summary())
+
+# diagnosis
+history = model.fit(X_train, y_train, epochs=20, batch_size=64, validation_split = 0.33)
+pyplot.plot(history.history['loss'])
+pyplot.plot(history.history['val_loss'])
+pyplot.title('model train vs validation loss')
+pyplot.ylabel('loss')
+pyplot.xlabel('epoch')
+pyplot.legend(['train', 'validation'], loc='upper right')
+pyplot.show()
+
+
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+print(model.predict(X_test))
