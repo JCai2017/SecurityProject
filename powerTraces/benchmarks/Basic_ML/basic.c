044332c69388d2366b5f08b9ba0ab5526a502d01
Jason Cai <jcai@ubuntu.(none)>
2017-12-04 20:57:20 -0600

    Created folder for ML stuff

---
+++ powerTraces/benchmarks/Basic_ML/basic.c
@@ -0,0 +1,34 @@
+#include<stdint.h>
+#include "key.h"
+
+int main(void){
+  volatile uint32_t x;
+  volatile uint32_t n;
+  volatile uint32_t y;
+  int i;
+  volatile uint32_t base = 3;
+  volatile uint32_t expo0 = 4;
+  volatile uint32_t expo1 = 5;
+  volatile uint32_t result = 1;
+
+  while(key != 0){
+    if(key % 2 == 0){
+      for(i = 0; i < 5000; i ++){
+        x = base * base;
+        n = key >> 1;
+      }
+      base = base * base;
+      key = key >> 1;
+    } else {
+      for(i = 0; i < 5000; i ++){
+        y = base * result;
+        x = base * base;
+        n = key >> 1;
+      }
+
+      result = base * result;
+      base = base * base;
+      key = key >> 1;
+    }
+  }
+}
