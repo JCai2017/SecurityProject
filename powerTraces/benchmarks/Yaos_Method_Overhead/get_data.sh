a30069f400d82c8011608ec1e874e69738e15b7d
Jason Cai <jason.cai.plano@gmail.com>
2017-12-14 19:10:07 -0600

    Yao's Overhead

--- powerTraces/benchmarks/Yaos_Method_Overhead/get_data.sh
+++ powerTraces/benchmarks/Yaos_Method_Overhead/get_data.sh
@@ -4,7 +4,7 @@ for ((i=0;i<7;i++));
 do
 	./createKeyHFile.py $key
 	clang -O0 -o yao yao.c 
-	../../../sniper/run-sniper --power --viz ./slidingWindow
+	../../../sniper/run-sniper --power --viz ./yao
 	./power_parser_class.py viz/levels/level2/data/Yaos_Method_Overhead-power.json data.csv
 
 	key=$((key * 2))
